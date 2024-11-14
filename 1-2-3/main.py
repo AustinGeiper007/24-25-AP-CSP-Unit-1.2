3818ee30057438b399b1b4538a8ef825c7d03fcf
AustinG <austin.geisler@vtsd.com>
2024-11-14 13:48:56 -0500

    1-2-3 Step8 Complete

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -8,8 +8,9 @@ wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.bgpic("background.gif")
 wn.addshape(apple_image) # Make the screen aware of the new file
-
 apple = trtl.Turtle()
+
+apple_fall_speed = 1
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
@@ -17,8 +18,16 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
+def apple_down(active_apple):
+  global apple_fall_speed
+  active_apple.penup()
+  xcor = active_apple.xcor()
+  ycor = active_apple.ycor()
+  ycor -= apple_fall_speed
+  active_apple.goto(xcor, ycor)
 
 #-----function calls-----
 draw_apple(apple)
-
+while True:
+  apple_down(apple)
 wn.mainloop()
