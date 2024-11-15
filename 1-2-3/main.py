7cba6cdcf6952cc2260ed54e9c66ad00da01b7a7
AustinG <austin.geisler@vtsd.com>
2024-11-15 12:55:32 -0500

    Step 9. Apple falls on keypress of a. Also simplified the drop_apple cmd

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -1,5 +1,6 @@
 #   a123_apple_1.py
 import turtle as trtl
+import random as rd
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
@@ -10,7 +11,8 @@ wn.bgpic("background.gif")
 wn.addshape(apple_image) # Make the screen aware of the new file
 apple = trtl.Turtle()
 
-apple_fall_speed = 1
+apple_fall_speed = 2.75 # Default is 2.75 (decimals work)
+ground_height = -200 # Default is -200
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
@@ -18,16 +20,15 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
-def apple_down(active_apple):
-  global apple_fall_speed
-  active_apple.penup()
-  xcor = active_apple.xcor()
-  ycor = active_apple.ycor()
-  ycor -= apple_fall_speed
-  active_apple.goto(xcor, ycor)
+def drop_apple():
+  apple.penup()
+  apple.speed(apple_fall_speed)
+  apple.goto(apple.xcor(), ground_height)
 
 #-----function calls-----
 draw_apple(apple)
-while True:
-  apple_down(apple)
+wn.onkeypress(drop_apple, "a")
+
+wn.listen() # Tells the window to look for keypresses
+
 wn.mainloop()
