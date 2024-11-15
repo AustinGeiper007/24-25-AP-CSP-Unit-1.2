97e0995c20b2872468b6bba4463085bab7dd2a1d
AustinG <austin.geisler@vtsd.com>
2024-11-15 13:11:49 -0500

    Trying and failing at step 11

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -10,19 +10,28 @@ wn.setup(width=1.0, height=1.0)
 wn.bgpic("background.gif")
 wn.addshape(apple_image) # Make the screen aware of the new file
 apple = trtl.Turtle()
+letter_pen = trtl.Turtle
 
 apple_fall_speed = 2.75 # Default is 2.75 (decimals work)
 ground_height = -200 # Default is -200
+font_setup = ("Arial", 55, "bold")
+font_color = "white"
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
+def draw_letter(active_apple):
+  active_apple.color(font_color)
+  active_apple.write("A", font=font_setup)
+
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
+  active_apple.penup()
+  draw_letter(active_apple)
   wn.update()
 
 def drop_apple():
-  apple.penup()
   apple.speed(apple_fall_speed)
+  apple.clear()
   apple.goto(apple.xcor(), ground_height)
 
 #-----function calls-----
