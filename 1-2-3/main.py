1c10caa71e1f31ea728c3cf9a36d2ddaa152fd71
AustinG <austin.geisler@vtsd.com>
2024-11-18 12:12:42 -0500

    main.py made to match github repo (I was behind)

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -3,41 +3,50 @@ import turtle as trtl
 import random as rd
 
 #-----setup-----
+import turtle as trtl
+
 apple_image = "apple.gif" # Store the file name of your shape
+ground_height = -200
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
-wn.bgpic("background.gif")
 wn.addshape(apple_image) # Make the screen aware of the new file
-apple = trtl.Turtle()
-letter_pen = trtl.Turtle
 
-apple_fall_speed = 2.75 # Default is 2.75 (decimals work)
-ground_height = -200 # Default is -200
-font_setup = ("Arial", 55, "bold")
-font_color = "white"
+wn.bgpic("background.gif")
+apple = trtl.Turtle()
+apple.penup()
+wn.tracer(False)
 
 #-----functions-----
 # given a turtle, set that turtle to be shaped by the image file
-def draw_letter(active_apple):
-  active_apple.color(font_color)
-  active_apple.write("A", font=font_setup)
-
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
-  active_apple.penup()
-  draw_letter(active_apple)
+  draw_letter("A", active_apple)
   wn.update()
 
+# This function moves the apple to the ground and hides it.
 def drop_apple():
-  apple.speed(apple_fall_speed)
-  apple.clear()
+  wn.tracer(True)
   apple.goto(apple.xcor(), ground_height)
+  apple.clear()
+  apple.hideturtle()
+  wn.tracer(False)
+
+
+# letter is of type str
+# active_apple is a turtle
+def draw_letter(letter, active_apple):
+  active_apple.color("white")
+  remember_position = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + apple_letter_x_offset,active_apple.ycor() + apple_letter_y_offset)
+  active_apple.write(letter, font=("Arial", 74, "bold"))
+  active_apple.setpos(remember_position)
 
 #-----function calls-----
 draw_apple(apple)
 wn.onkeypress(drop_apple, "a")
 
 wn.listen() # Tells the window to look for keypresses
-
 wn.mainloop()
