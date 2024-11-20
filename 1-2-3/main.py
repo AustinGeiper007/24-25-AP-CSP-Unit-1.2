b28a158c53a6e5f7c56390673c71562d8abf841b
AustinG <austin.geisler@vtsd.com>
2024-11-20 13:43:59 -0500

    Removed the TODO comments, that were complete but not deleted. Apple Avalanche now checks for the letter. Typing that out made me cry :( Also minor changes to the syntax and methodology of certain functions

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -62,31 +62,6 @@ def reset_apple(apple):
       draw_apple(apple_letters[0], apple)
 
 
-#TODO Create a function that takes a turtle (apple) and its corresponding letter from the letter
-# list and set that turtle to be shaped by the image file, call the letter drawing function,
-# and update the Screen
-
-#TODO Iterate over the numbers from 0 to the number of apples, creating that many turtles
-# calling your function that resets the apples by giving them a new random location
-# add the new apples to a list of apples to be used in the rest of the program.
-# The loop below executes the correct number of times by using the range() function
-# to create a list of numbers to iterate over.
-#for i in range(0, number_of_apples):
-  #Your code here
-
-#TODO Create a function that takes a letter as its parameter, uses that letter to retrieve the
-# corresponding turtle (apple) and causes both to drop from the tree simultaneously. Once the
-# apple and letter have dropped, call the apple resetting function.
-
-#TODO define a function per letter that you will use in your program. Each function should check
-# to see if the given letter is in the list of letters; if it is, it should drop the corresponding
-# apple.
-
-#TODO use the onkeypress method of wn to correlate the functions you defined above with each
-# of the letters that the user might type.
-# onkeypress requires that you name one function that must take
-# no arguments to be called when the specified key is pressed.
-
 def check_apple_a():
     if ('a' in apple_letters):
         drop_apple()
